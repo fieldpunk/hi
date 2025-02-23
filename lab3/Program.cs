ef3cef7d13aa8f3b4c8a2f3d7c5179a34020f63d
Adilbek Esengeldinov <20klip05@gmail.com>
2025-02-23 11:28:22 +0000

    First

---
+++ lab3/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Run(async(context)=>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    if (context.Request.Path == "/postuser")
+    {
+        var form = context.Request.Form;
+        string name = form["name"];
+        string age = form["age"];
+        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
+    }
+    else
+    {
+        await context.Response.SendFileAsync("html/index.html");
+    }
+} );
+
+app.Run();
